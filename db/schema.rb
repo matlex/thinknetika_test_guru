9399cf2e38f7b7540e5f30263f2f9b3839c74a9d
Matvey Ustinov <mat.ustinov@gmail.com>
2019-01-16 10:53:16 +0600

    Миграция на добавление колонки correct в таблице Answer,  и установление значения по-умолчанию false при создании записи Answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_041111) do
+ActiveRecord::Schema.define(version: 2019_01_16_044533) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "correct", default: false
   end
 
   create_table "categories", force: :cascade do |t|
