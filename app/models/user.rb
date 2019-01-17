e9260c86e993e2af90e2541daf09671f35491e5e
Matvey Ustinov <mat.ustinov@gmail.com>
2019-01-18 01:18:48 +0600

    Создать инстанс-метод в модели User, который принимает в качестве аргумента значение уровня сложности и возвращает список всех Тестов, которые проходит или когда-либо проходил Пользователь на этом уровне сложности

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def get_tests_by_level(level_name)
+    Test.where("level = :level_name AND user_id = :user_id", level_name: level_name, user_id: self.id)
+  end
 end
