74da0ece9365df07ca9bf29d4aa377050f854d92
Matvey Ustinov <mat.ustinov@gmail.com>
2019-01-18 20:58:58 +0600

    Добавить связь один ко многим между Тестами и Категориями

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_042117) do
+ActiveRecord::Schema.define(version: 2019_01_18_042749) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2019_01_17_042117) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "level", default: "beginner", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
