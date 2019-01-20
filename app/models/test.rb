c8c0e296a07291a0d3c27a8ff46a8bbd7dcecbf3
Matvey Ustinov <mat.ustinov@gmail.com>
2019-01-20 20:12:59 +0600

    Возвращать только названия тестов. Сортировка по названиям тестов.

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.get_by_category(title)
-    joins(:category).where(categories: {title: title}).order('id DESC')
+    joins(:category).where(categories: {title: title}).order('tests.title DESC').pluck("tests.title")
   end
 
 end
