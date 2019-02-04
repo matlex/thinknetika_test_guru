5283a6aa64da19b1683ee3a62e1a64ef824eb8fc
Matvey Ustinov <mat.ustinov@gmail.com>
2019-02-05 00:08:27 +0600

    Добавить ассоциацию между Тестами и проходящими их Пользователями (учесть, что один и тот же тест могут проходить несколько пользователей одновременно)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,10 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author
+
   has_many   :questions
+  has_many   :passed_tests
+  has_many   :users, through: :passed_tests
 
   def self.get_by_category(title)
     joins(:category).where(categories: {title: title}).order('tests.title DESC').pluck("tests.title")
