c8a9443ce36773ff208d9b2862e4ab4d41ec3506
Matvey Ustinov <mat.ustinov@gmail.com>
2019-02-04 19:40:12 +0600

    Добавить ассоциацию между Тестами и Автором

---
+++ db/migrate/20190204132907_add_author_ref_to_tests.rb
@@ -0,0 +1,5 @@
+class AddAuthorRefToTests < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :tests, :author, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_091524) do
+ActiveRecord::Schema.define(version: 2019_02_04_132907) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_02_04_091524) do
     t.boolean "correct", default: false
     t.integer "question_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -48,6 +55,8 @@ ActiveRecord::Schema.define(version: 2019_02_04_091524) do
     t.datetime "updated_at", null: false
     t.string "level", default: "beginner", null: false
     t.integer "category_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
