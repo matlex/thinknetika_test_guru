bcdb35d35b1132d4c4d2643ea158f6a86814df66
Matvey Ustinov <mat.ustinov@gmail.com>
2019-02-04 15:30:17 +0600

    Добавить в качестве заметки, команду, использующуюся для генерации миграции

--- db/migrate/20190204083412_add_test_id_to_questions.rb
+++ db/migrate/20190204083412_add_test_id_to_questions.rb
@@ -1,5 +1,6 @@
 class AddTestIdToQuestions < ActiveRecord::Migration[5.2]
   def change
+    # rails g migration add_test_id_to_questions test_id:integer
     add_column :questions, :test_id, :integer
     add_index  :questions, :test_id
   end
