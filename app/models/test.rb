e493dcfa45cb2f28f7c4c365c1bf81b337ede39a
Matvey Ustinov <mat.ustinov@gmail.com>
2019-02-04 19:40:12 +0600

    Добавить ассоциацию между Тестами и Автором

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
+  belongs_to :author
+  has_many   :questions
 
   def self.get_by_category(title)
     joins(:category).where(categories: {title: title}).order('tests.title DESC').pluck("tests.title")
