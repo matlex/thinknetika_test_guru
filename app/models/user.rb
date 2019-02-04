35ad1d0af2dcf217a373a70251e24e3178db4bad
Matvey Ustinov <mat.ustinov@gmail.com>
2019-02-05 00:08:27 +0600

    Добавить ассоциацию между Тестами и проходящими их Пользователями (учесть, что один и тот же тест могут проходить несколько пользователей одновременно)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :passed_tests
+  has_many :tests, through: :passed_tests
+
   def passed_tests_by_level(level_name)
     Test.joins("JOIN passed_tests ON tests.id = passed_tests.test_id AND passed_tests.user_id = #{self.id}").where(level: level_name)
   end
