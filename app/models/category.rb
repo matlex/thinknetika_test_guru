19654e0bfb4793202821398a9785aa8514e75d10
Matvey Ustinov <mat.ustinov@gmail.com>
2019-02-15 18:26:22 +0600

    Добавить валидацию title на уникальность у Категории на уровне приложения и на уровне БД

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   has_many :tests
 
   default_scope { order(title: :asc ) }
+
+  validates :title, uniqueness: true,
+                    presence: true
 end
---
+++ db/migrate/20190215100748_add_uniqueness_to_categories_title.rb
@@ -0,0 +1,5 @@
+class AddUniquenessToCategoriesTitle < ActiveRecord::Migration[5.2]
+  def change
+    add_index :categories, :title, unique: true
+  end
+end
