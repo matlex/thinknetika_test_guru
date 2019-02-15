39e51b16fb0f856464e44d4eece7256ae89b5f1a
Matvey Ustinov <mat.ustinov@gmail.com>
2019-02-15 18:29:35 +0600

    Добавить валидацию полей модели Answer. А так же добавить кастомную валидацию validate_answers_count_in_question для запрета сохранения Ответа в Вопросе, который уже имеет 4 и более ответов.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,18 @@
 class Answer < ApplicationRecord
   belongs_to :question
   scope      :all_correct, -> { where(correct: true) }
+
+  validates :body, presence: true
+  validates :question, presence: true
+
+  validate :validate_answers_count_in_question
+
+  private
+
+  def validate_answers_count_in_question
+    if question&.answers
+      answers_count = question.answers.count
+      errors.add(:question, "Too many answers(#{answers_count.to_s}) added to question with id: #{question.id}") if answers_count >= 4
+    end
+  end
 end
