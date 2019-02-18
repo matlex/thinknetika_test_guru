50122f15afd044f46d75e07b053cab71cdf28ff8
Matvey Ustinov <mat.ustinov@gmail.com>
2019-02-18 10:27:29 +0600

    Отптимизация запроса проверки наличия записей в БД

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -10,7 +10,7 @@ class Answer < ApplicationRecord
   private
 
   def validate_answers_count_in_question
-    if question&.answers
+    if question&.answers.exists?
       answers_count = question.answers.count
       errors.add(:question, "Too many answers(#{answers_count.to_s}) added to question with id: #{question.id}") if answers_count >= 4
     end
