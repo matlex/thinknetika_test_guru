93a4a36eb73f24fa45c7ab639234c7e09b354fae
Matvey Ustinov <mat.ustinov@gmail.com>
2019-05-22 23:08:11 +0600

    Создание логики приложения: добазвить роуты для старта прохождения теста, для ресурса passed_tests  а так же для просмотра результата прохождения тестов

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,16 @@ Rails.application.routes.draw do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
     end
+
+    member do
+      post :start
+    end
+  end
+
+  resources :passed_tests, only: %i[show update] do
+    member do  # GET /passed_tests/101/result
+      get :result
+    end
   end
 
 end
