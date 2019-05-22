0d1037d9cd5d1687d23daebe8623c1d1ac686c15
Matvey Ustinov <mat.ustinov@gmail.com>
2019-05-22 23:10:59 +0600

    Создание логики приложения: реализация основных методов модели PassedTest(TestPassage)

--- app/models/passed_test.rb
+++ app/models/passed_test.rb
@@ -1,5 +1,54 @@
 class PassedTest < ApplicationRecord
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question
+  belongs_to :current_question, class_name: 'Question', optional: true
+
+  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+
+    self.save!
+  end
+
+  def completed?
+    current_question.nil?
+  end
+
+  def rate_percentage
+    completed? ? ((self.correct_questions.to_f / questions_count) * 100).to_i : 0
+  end
+
+  def question_number
+    test.questions.pluck(:id).index(current_question.id) + 1
+  end
+
+  def questions_count
+    test.questions.count
+  end
+
+  private
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_question
+    self.current_question = next_question
+  end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    self.test.questions.order(:id).where('id > ?', current_question.id).first
+  end
 end
