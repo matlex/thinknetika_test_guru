16a71fd362cc388d3f80c24ebf87705619736335
Matvey Ustinov <mat.ustinov@gmail.com>
2019-05-30 20:28:03 +0600

    Пользовательские сессии: реализовать контроллер Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'tests#index'
+
+  get :signup, to: 'users#new'
+
+  resources :users, only: :create
 
   resources :tests do
     resources :questions, shallow: true, except: :index do
