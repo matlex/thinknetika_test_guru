3785c843a2d013ee537d8b27f1f0a7c3e5ecba6b
Matvey Ustinov <mat.ustinov@gmail.com>
2019-06-02 21:19:05 +0600

    Пользовательские сессии: работа над замечаниями. Убрать метод set_user т.к. есть новый метод current_user в родительском классе

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,6 @@ class TestsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :set_test, only: %i[show start]
-  before_action :set_user, only: :start
 
   def show
   end
@@ -12,17 +11,13 @@ class TestsController < ApplicationController
   end
 
   def start
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
 
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = User.find(session[:user_id])
   end
 end
