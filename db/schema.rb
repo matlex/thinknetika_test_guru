2b8964ede4bd4ee048cc7ab2b5fe93f05d67ad09
Matvey Ustinov <mat.ustinov@gmail.com>
2019-06-09 22:38:37 +0600

    Devise Auth: Добавить колонку type в таблицу Users тем самым реализовав паттерн STI над таблицей users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_172248) do
+ActiveRecord::Schema.define(version: 2019_06_09_163434) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -73,9 +73,11 @@ ActiveRecord::Schema.define(version: 2019_06_06_172248) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "type", default: "User", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["type"], name: "index_users_on_type"
   end
 
 end
