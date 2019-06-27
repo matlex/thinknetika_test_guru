f90a10a9b3d3a42cef9880d11aae32d74076eab1
Matvey Ustinov <mat.ustinov@gmail.com>
2019-06-27 10:45:12 +0600

    Локализация в Rails: добавить файлы с локалями и произвести переводы ресурсов тестов для пользователя, тестов для администратора, прохождения тестов для пользователя Локализовать представления Devise Установить перевод для подсказки в списке категорий при создании нового теста

--- app/helpers/passed_tests_helper.rb
+++ app/helpers/passed_tests_helper.rb
@@ -1,7 +1,7 @@
 module PassedTestsHelper
 
   def show_current_question_num_of_total
-    output_string = "<h2>Question # #{@current_test_passage.question_number} of #{@current_test_passage.questions_count}</h2>"
+    output_string = I18n.t('.question_num_of_total_html', question_number: @current_test_passage.question_number, total: @current_test_passage.questions_count)
     output_string.html_safe
   end
 
