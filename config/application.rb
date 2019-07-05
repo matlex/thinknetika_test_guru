33a8b06e9c3134ece11e3fad4dd06fabd6d65ace
Matvey Ustinov <mat.ustinov@gmail.com>
2019-07-05 19:53:55 +0600

    External APIs: remove dotenv misc config

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-HOSTNAME = ENV['HOSTNAME']
-
 module ThinknetikaTestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
