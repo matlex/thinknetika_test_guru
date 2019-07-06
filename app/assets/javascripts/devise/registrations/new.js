86c64fe3af0d30a54d1f603961c44d7c73da226c
Matvey Ustinov <mat.ustinov@gmail.com>
2019-07-06 20:33:30 +0600

    JavaScript in Rails app: Implement validation logic with JS for password fields on SignUp Page

---
+++ app/assets/javascripts/devise/registrations/new.js
@@ -0,0 +1,38 @@
+document.addEventListener('turbolinks:load', function() {
+    var password_field = document.getElementById('user_password');
+    var confirmation_password_field = document.getElementById('user_password_confirmation');
+
+    password_field.addEventListener('input', validate);
+    confirmation_password_field.addEventListener('input', validate);
+
+    function validate() {
+        // remove validation classes when both fields or confirmation field is empty
+        if (!password_field.value && !confirmation_password_field.value || password_field.value && !confirmation_password_field.value) {
+            clearAllValidations();
+        } else if (password_field.value && confirmation_password_field.value) {
+            if (password_field.value == confirmation_password_field.value) {
+                password_field.classList.remove('is-invalid');
+                confirmation_password_field.classList.remove('is-invalid');
+
+                password_field.classList.add('is-valid');
+                confirmation_password_field.classList.add('is-valid');
+            } else {
+                password_field.classList.remove('is-valid');
+                password_field.classList.add('is-invalid');
+
+                confirmation_password_field.classList.remove('is-valid');
+                confirmation_password_field.classList.add('is-invalid');
+            }
+        } else if (!password_field.value && confirmation_password_field) {
+            confirmation_password_field.classList.add('is-invalid');
+        }
+    }
+
+    function clearAllValidations() {
+        password_field.classList.remove('is-valid');
+        password_field.classList.remove('is-invalid');
+
+        confirmation_password_field.classList.remove('is-valid');
+        confirmation_password_field.classList.remove('is-invalid')
+    }
+});
