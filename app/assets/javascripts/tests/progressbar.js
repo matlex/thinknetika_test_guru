213583e047454e3eeca3fe9a14defac02fed51d9
Matvey Ustinov <mat.ustinov@gmail.com>
2019-07-08 21:28:23 +0600

    Data attributes in HTML: misc

--- app/assets/javascripts/tests/progressbar.js
+++ app/assets/javascripts/tests/progressbar.js
@@ -2,7 +2,7 @@ document.addEventListener('turbolinks:load', function(){
     const progressBar = document.querySelector('.progress-bar');
 
     if (progressBar) {
-        const progressPercentage =progressBar.dataset.progressPercentage;
+        const progressPercentage = progressBar.dataset.progressPercentage;
 
         progressBar.setAttribute('aria-valuenow', progressPercentage);
         progressBar.setAttribute('style', `width: ${progressPercentage}%`)
